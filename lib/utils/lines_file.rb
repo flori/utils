6700a5d28f73dff941e2d4d20f68e62d7ae8d4f9
Florian Frank <flori@ping.de>
2011-08-09 22:11:21 +0200

    Add LinesFile to handle line navigation in files

--- lib/utils/lines_file.rb
+++ lib/utils/lines_file.rb
@@ -1,29 +1,122 @@
 class Utils::LinesFile
-  def self.for_filename(filename, linenumber = nil)
-    new(File.new(filename, 'r'), linenumber)
+  module LineExtension
+    attr_reader :line_number
+
+    def filename
+      lines_file.filename.dup
+    end
   end
-  
-  def initialize(file, linenumber)
-    @file = file
-    @linenumber = (linenumber || 1).to_i
+
+  include Enumerable
+
+  def self.for_filename(filename, line_number = nil)
+    obj = new(File.readlines(filename), line_number)
+    obj.filename = filename
+    obj
   end
-  
-  attr_accessor :linenumber
+
+  def self.for_file(file, line_number)
+    obj = new(file.readlines, line_number)
+    obj.filename = filename
+    obj
+  end
+
+  def self.for_lines(lines, line_number = nil)
+    new(lines, line_number)
+  end
+
+  def initialize(lines, line_number = nil)
+    @lines = lines
+    @lines.each_with_index do |line, i|
+      line.extend LineExtension
+      line.instance_variable_set :@line_number, i + 1
+      line.instance_variable_set :@lines_file, self
+    end
+    self.line_number = line_number || @lines.empty? ? 0 : 1
+  end
+
+  attr_accessor :filename
+
+  attr_reader :line_number
+
+  def rewind
+    self.line_number = 1
+    self
+  end
+
+  def next!
+    self.line_number += 1
+    self
+  end
+
+  def previous!
+    self.line_number -= 1
+    self
+  end
+
+  def line_number=(number)
+    number = number.to_i
+    if number > 0 && number <= last_line_number
+      @line_number = number
+    end
+  end
+
+  def last_line_number
+    @lines.size
+  end
+
+  def empty?
+    @lines.empty?
+  end
+
+  def each(&block)
+    @lines.empty? and return
+    old_line_number = line_number
+    1.upto(last_line_number) do |number|
+      self.line_number = number
+      block.call(line)
+    end
+    self
+  ensure
+    self.line_number = old_line_number
+  end
 
   def line
-    index = @linenumber.to_i - 1
-    @file.rewind
-    @file.each_with_index do |line, i|
-      index == i and return line
-    end
-    nil
+    index = line_number - 1
+    @lines[index] if index >= 0
   end
 
-  def match_backward(regexp)
-    @linenumber = @linenumber.to_i
-    while @linenumber > 0
-      line =~ regexp and return $~.captures
-      @linenumber -= 1
+  def file_linenumber
+    "#{filename}:#{line_number}"
+  end
+
+  def match_backward(regexp, previous_after_match = false)
+    empty? and return
+    while line_number >= 1
+      if line =~ regexp
+        previous_after_match and previous!
+        return $~.captures
+      end
+      previous!
     end
+  end
+
+  def match_forward(regexp, next_after_match = false)
+    empty? and return
+    begin
+      if line =~ regexp
+        next_after_match and next!
+        return $~.captures
+      end
+      next!
+    end while line_number < last_line_number
+  end
+
+  def to_s
+    "#{line_number} #{line.chomp}"
+  end
+
+  def inspect
+    "#<#{self.class}: #{to_s.inspect}>"
   end
 end
