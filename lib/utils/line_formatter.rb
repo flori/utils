4f5e1baaaf3f3c514d85de911c5f59dfca4b2a1b
Florian Frank <flori@ping.de>
2015-06-17 12:16:58 +0200

    Use newest RSpec formatter interface

--- lib/utils/line_formatter.rb
+++ lib/utils/line_formatter.rb
@@ -3,109 +3,155 @@ require 'term/ansicolor'
 
 begin
   require 'rspec/core'
-  require 'rspec/core/formatters/base_text_formatter'
+  require 'rspec/core/formatters'
 rescue LoadError => e
-  if $DEBUG
-    warn "Caught: #{e.class}: #{e}"
-  end
+  $DEBUG and warn "Caught #{e.class}: #{e}"
 else
   module Utils
-    class LineFormatter < RSpec::Core::Formatters::BaseTextFormatter
-      def start(example_count)
-        super
-        filename = 'errors.lst'
-        output.puts "Storing error list in #{filename.inspect}: "
-        @errors_lst = File.new(filename, 'w')
-        @errors_lst.sync = true
-      end
+    class LineFormatter
+        ::RSpec::Core::Formatters.register self, :start, :close,
+          :example_passed, :example_pending, :example_failed, :dump_summary
 
-      def close
-        super
-        @errors_lst.puts "\n#{?= * 75}\nFinished in #{format_duration(duration)}\n"
-        @errors_lst.puts summary_line(example_count, failure_count, pending_count)
-        @errors_lst.close
-      end
-
-      def example_passed(example)
-        super
-        output.puts format_line(example)
-      end
-
-      def example_pending(example)
-        super
-        output.puts format_line(example)
-      end
-
-      def example_failed(example)
-        super
-        dump_line_to_error_file(example)
-        output.puts format_line(example)
-        dump_failure_info(example)
-      end
-
-      def dump_failures
-      end
-
-      def dump_pending
-      end
-
-      def dump_commands_to_rerun_failed_examples
-      end
-
-      private
-
-      def dump_failure_for_error_file(example)
-        result = ''
-        exception = example.execution_result[:exception]
-        exception_class_name = exception_class_name_for(exception)
-        result << "#{long_padding}Failure/Error: #{read_failed_line(exception, example).strip}\n"
-        result << "#{long_padding}#{exception_class_name}:\n" unless exception_class_name =~ /RSpec/
-        exception.message.to_s.split("\n").each { |line| result << "#{long_padding}  #{line}\n" } if exception.message
-        result
-      end
-
-      def dump_line_to_error_file(example)
-        @errors_lst.flock File::LOCK_EX
-        @errors_lst.puts "%s\n%3.3fs %s\n%s\n%s" % [
-          location(example), run_time(example), example.full_description,
-          Term::ANSIColor.uncolored(dump_failure_for_error_file(example)),
-          (%w[ {{{ ] +
-           format_backtrace(example.execution_result[:exception].backtrace, example) +
-           %w[ }}} ]) * ?\n
-        ]
-      ensure
-        @errors_lst.flock File::LOCK_UN
-      end
-
-      def run_time(example)
-        example.execution_result[:run_time]
-      end
-
-      def format_line(example)
-        description =
-          if ENV['VERBOSE'].to_i == 1
-            example.full_description
-          else
-            example.description
-          end
-        args = [ location(example), run_time(example), description ]
-        uncolored = "%s # S %3.3fs %s" % args
-        uncolored = uncolored[0, Tins::Terminal.columns]
-        case example.execution_result[:status]
-        when 'passed'
-          success_color(uncolored)
-        when 'failed'
-          failure_color(uncolored)
-        when 'pending'
-          pending_color(uncolored)
-        else
-          uncolored % args
+        def initialize(output)
+          @output = output
+          filename = 'errors.lst'
+          @errors_lst = File.new(filename, 'w')
+          @errors_lst.sync = true
         end
-      end
 
-      def location(example)
-        RSpec::Core::Metadata::relative_path(example.location)
-      end
+        attr_reader :output
+
+        def start(_ignore)
+          output.puts "Storing error list in #{@errors_lst.path.inspect}: "
+        end
+
+        def close(_ignore)
+          @errors_lst.close
+        end
+
+        def dump_summary(summary)
+          @errors_lst.puts "\n#{?= * 75}\nFinished in %s\n" %
+            format_duration(summary.duration)
+          @errors_lst.puts line = summary_line(summary)
+          output.puts line
+        end
+
+        def example_passed(example)
+          output.puts format_line(example)
+        end
+
+        def example_pending(example)
+          output.puts format_line(example)
+        end
+
+        def example_failed(example)
+          dump_failure_to_error_file(example)
+          output.puts format_line(example)
+        end
+
+        private
+
+        def summary_line(example)
+          "%u of %u (%.2f %%) failed, %u pending (%.2f %%)" % [
+            example.failure_count,
+            example.example_count,
+            100 * example.failure_count.to_f / example.example_count,
+            example.pending_count,
+            100 * example.pending_count.to_f / example.example_count,
+          ]
+        end
+
+        def format_duration(duration)
+          "%0.3f seconds" % duration
+        end
+
+        def read_failed_line(example)
+          ''.strip
+        end
+
+        def dump_failure_for_error_file(example)
+          result = ''
+          exception = execution_result(example).exception
+          exception_class_name = exception.class.name
+          result << "Failure/Error: #{read_failed_line(example)}\n"
+          result << "#{exception_class_name}:\n" unless exception_class_name =~ /RSpec/
+          if m = exception.message
+            m.to_s.split("\n").each { |line| result << "  #{line}\n" }
+          end
+          result
+        end
+
+        def format_backtrace(example)
+          backtrace = execution_result(example).exception.backtrace
+          result = %w[ {{{ ]
+          for line in backtrace
+            result << RSpec::Core::Metadata::relative_path(line)
+          end
+          result += %w[ }}} ]
+          result * ?\n
+        end
+
+        def dump_failure_to_error_file(example)
+          @errors_lst.flock File::LOCK_EX
+          @errors_lst.puts "%s\n%3.3fs %s\n%s\n%s" % [
+            location(example), run_time(example), description(example, full: true),
+            dump_failure_for_error_file(example), format_backtrace(example)
+          ]
+        ensure
+          @errors_lst.flock File::LOCK_UN
+        end
+
+        def execution_result(example)
+          example.example.metadata[:execution_result]
+        end
+
+        def description(example, full: ENV['VERBOSE'].to_i == 1)
+          if full
+            example.example.full_description
+          else
+            example.example.description
+          end
+        end
+
+        def run_time(example)
+          execution_result(example).run_time
+        end
+
+        def format_line(example)
+          args = [ location(example), run_time(example), description(example) ]
+          uncolored = "%s # S %3.3fs %s" % args
+          uncolored = uncolored[0, Tins::Terminal.columns]
+          case execution_result(example).status
+          when :passed
+            success_color(uncolored)
+          when :failed
+            failure_color(uncolored)
+          when :pending
+            pending_color(uncolored)
+          else
+            uncolored % args
+          end
+        end
+
+        def success_color(text)
+          Term::ANSIColor.green(text)
+        end
+
+        def failure_color(text)
+          Term::ANSIColor.red(text)
+        end
+
+        def pending_color(text)
+          Term::ANSIColor.yellow(text)
+        end
+
+        def location(example)
+          location = example.example.metadata[:location]
+          unless location.include?(?/)
+            location = example.example.metadata[:example_group][:location]
+          end
+          RSpec::Core::Metadata::relative_path(location)
+        end
     end
   end
 end
