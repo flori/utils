e24f49753c0e68f0e1ac123938c007e02dc1b374
Florian Frank <flori@ping.de>
2016-12-08 12:45:26 +0100

    Move the meat of this into its own method

--- lib/utils/line_blamer.rb
+++ lib/utils/line_blamer.rb
@@ -8,6 +8,14 @@ module Utils
       location = line.source_location and new *location
     end
 
+    def self.blame(line)
+      blamer = for_line(line)
+      if blame = blamer.perform
+        blame.sub!(/^[0-9a-f^]+/) { Term::ANSIColor.yellow($&) }
+        blame.sub!(/\(([^)]+)\)/) { "(#{Term::ANSIColor.red($1)})" }
+      end
+    end
+
     def perform(options = '')
       `git 2>/dev/null blame #{options} -L #@lineno,+1 "#@file"`.full?
     end
