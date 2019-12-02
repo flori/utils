a7d42ff239696c8b6913a64beeb7b83ca0aded2d
Florian Frank <flori@ping.de>
2019-12-02 14:48:48 +0100

    Add selector for result list

--- lib/utils/search_ui.rb
+++ lib/utils/search_ui.rb
@@ -4,6 +4,7 @@ class Utils::SearchUI
   include Term::ANSIColor
 
   def initialize(query:, found:, output: STDOUT, prompt: 'Search? %s')
+    @selector = 0
     @query  = query
     @found  = found
     @output = output
@@ -18,7 +19,7 @@ class Utils::SearchUI
       case getc
       when true
         @output.print clear_screen, move_home, reset
-        if result = @found.(@answer)
+        if result = @found.(@answer, @selector)
           return result
         else
           return nil
@@ -26,7 +27,7 @@ class Utils::SearchUI
       when false
         return nil
       end
-      result = @query.(@answer)
+      result = @query.(@answer, @selector)
       @output.print clear_screen
       unless @answer.empty?
         @output.print move_home { ?\n + result }
@@ -41,19 +42,32 @@ class Utils::SearchUI
     c = STDIN.getc
     system 'stty cooked echo'
     case c
-    when "\x03", "\e"
+    when "\x03"
       false
+    when "\e"
+      STDIN.getc == ?[ or return nil
+      STDIN.getc =~ /\A([AB])\z/ or return nil
+      if $1 == ?A
+        @selector -= 1
+      else
+        @selector += 1
+      end
+      @selector = [ @selector, 0 ].max
+      nil
     when ?\r
       true
     when "\x7f"
+      @selector = 0
       @answer.chop!
       nil
     when "\v"
+      @selector = 0
       @answer.clear
       nil
     when /\A[\x00-\x1f]\z/
       nil
     else
+      @selector = 0
       @answer << c
       nil
     end
