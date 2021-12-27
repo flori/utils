f05f8124ae79faf32ec318bbd2a50fc53c015622
Florian Frank <flori@ping.de>
2021-12-27 11:31:23 +0100

    fix

--- lib/utils/version.rb
+++ lib/utils/version.rb
@@ -1,6 +1,6 @@
 module Utils
   # Utils version
-  VERSION         = '0.24.0'
+  VERSION         = '0.24.1'
   VERSION_ARRAY   = VERSION.split('.').map(&:to_i) # :nodoc:
   VERSION_MAJOR   = VERSION_ARRAY[0] # :nodoc:
   VERSION_MINOR   = VERSION_ARRAY[1] # :nodoc:
