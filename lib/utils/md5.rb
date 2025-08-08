aaeebc80e83dfd430e5acbc3bf76cba53c143a61
Florian Frank <flori@ping.de>
2025-08-08 22:25:15 +0200

    Add documentation to buffer_size and md5 method

--- lib/utils/md5.rb
+++ lib/utils/md5.rb
@@ -3,12 +3,27 @@ require 'digest/md5'
 module Utils
   module MD5
     class << self
+      # The buffer_size accessor method provides read and write access to the
+      # buffer_size instance variable.
+      #
+      # @return [ Integer ] the current buffer size value
+      def buffer_size; end
       attr_accessor :buffer_size
     end
     self.buffer_size = 2 ** 20 - 1
 
     module_function
 
+    # Computes the MD5 hash digest for a given file.
+    #
+    # This method reads the entire contents of the specified file in binary
+    # mode and calculates its MD5 hash value. It uses a configurable buffer
+    # size for reading the file in chunks to optimize memory usage during the
+    # hashing process.
+    #
+    # @param filename [ String ] the path to the file for which to compute the MD5 hash
+    #
+    # @return [ String ] the hexadecimal representation of the MD5 hash digest
     def md5(filename)
       digest = Digest::MD5.new
       digest.reset
