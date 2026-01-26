9dea03bb34e632b53069d975c95f5ff6c02ef36b
Florian Frank <flori@ping.de>
2026-01-26 18:37:43 +0100

    Bump version to **0.92.0**
    - Updated `VERSION` file to **0.92.0**
    - Updated `Utils::VERSION` constant to **0.92.0**
    - Updated gemspec version to **0.92.0**

--- lib/utils/version.rb
+++ lib/utils/version.rb
@@ -1,6 +1,6 @@
 module Utils
   # Utils version
-  VERSION         = '0.91.1'
+  VERSION         = '0.92.0'
   VERSION_ARRAY   = VERSION.split('.').map(&:to_i) # :nodoc:
   VERSION_MAJOR   = VERSION_ARRAY[0] # :nodoc:
   VERSION_MINOR   = VERSION_ARRAY[1] # :nodoc:
